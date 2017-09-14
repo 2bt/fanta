fd9048c798aa90c988ccd3269344dcfe5284e9d5
2bt <s8572327@gmail.com>
2017-09-14 17:04:20 +0200

    resolve names

--- test.c
+++ test.c
@@ -4,15 +4,16 @@ int g;
 int i;
 
 
+void main() {
+   f = 5;
+   g = f() * f() * g();
+   i = 10 + 3 * 4;
+}
+
+
 int f() {
    f = f + 1;
    return f;
 }
 
-void main() {
-   f = 5;
-   g = f() * f() * f();
-   i = 10 + 3 * 4;
-}
-
-
+void g() {}
