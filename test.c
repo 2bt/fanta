dd5a38b11563193068cd6dff8e28bb637a3fa0b0
2bt <s8572327@gmail.com>
2017-09-18 16:44:35 +0200

    if

--- test.c
+++ test.c
@@ -1,20 +1,21 @@
-
+int a;
 int f;
 int g;
 int i;
 
 
 void main() {
+   a = 1;
    f = 5;
    foo();
-   g = f() * f() * g();
+   g = f() * g();
    i = 10 + 3 * 4;
 }
 
 
 int f() {
-   f = f + 1;
-   return f;
+   f = !f;
+   return 10;
 }
 
 int g() {
@@ -22,5 +23,6 @@ int g() {
 }
 
 void foo() {
-
+   if (a == 0) a = 10;
+   else a = 3 * 3;
 }
