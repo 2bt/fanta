23061eb81480409be6c25de004e12613128ede7c
2bt <s8572327@gmail.com>
2017-11-14 13:13:15 +0100

    enum

--- test.c
+++ test.c
@@ -1,15 +1,12 @@
+enum { CONSTANT = 42 };
 int i;
 int s;
-
-enum {
-   CONSTANT = 42,
-};
 
 //int[L] a;
 
 int main() {
+//   for (;;) i = i + 1;
    i = 10 * 4 + CONSTANT;
-
    for (i = CONSTANT; i; i = i - 1) s = s + i;
    return s;
 }
